52b26b04c51bb181c320c338e10f36b7ed9129b8
darkstar21 <mikeh09@gmail.com>
2014-09-19 20:47:25 -0700

    solution to core requirements

--- watchout.js
+++ watchout.js
@@ -1,30 +1,49 @@
 // start slingin' some d3 here.
 
   var getDistance = function(n1,n2){
-    var diffX = n1.x - n2.x;
-    var diffY = n1.y - n2.y;
+    var diffX = n1.x.animVal.value - n2.x.animVal.value;
+    var diffY = n1.y.animVal.value - n2.y.animVal.value;
 
-    return Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
-
+    var answer = Math.sqrt(Math.pow(diffX,2) + Math.pow(diffY,2));
+    return answer;
   }
 // var data = [1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6,1,2,3,4,5,6];
 function collide(node1,node2,callback) {
-  if(getDistance(node1,node2) < 50){
+  if(getDistance(node1,node2) < 80){
     callback.apply(this,arguments);
   }
-  console.log('This is the problem');
-
 };
 
 var tester = function(){
   console.log('It fucking WORKS!!!!!!!!');
 }
 
+var updateScoreBoard = function(){
+  d3.select('.scoreboard .high span').text(highScore.toString());
+  d3.select('.scoreboard .current span').text(currScore.toString());
+}
+var turnCollision = 0;
+var currScore = 0;
+var highScore = 0;
+
+var collisionVerified =function(){
+  turnCollision++;
+}
+
+var gameMaster = function(){
+  if(turnCollision > 0){
+    if(currScore > highScore){
+      highScore = currScore;
+    }
+    turnCollision = 0;
+    currScore = 0;
+    updateScoreBoard();
+  }
+}
 
 
-
-var height = 1000;
-var width = 1000;
+var height = 800;
+var width = 1900;
 
 var canvas = d3.select('body').append('svg')
                .attr('height',height)
@@ -39,12 +58,18 @@ var drag = d3.behavior.drag()
           .on("drag", function(d,i) {
             d.x += d3.event.dx
             d.y += d3.event.dy
-            d3.select(this).attr("transform", function(d,i){
-                return "translate(" + [ d.x,d.y ] + ")"
+            d3.select(this).attr('x', function(d){
+              return d.x;
             })
+            .attr('y', function(d){
+              return d.y;
+            })
+            // .attr("transform", function(d,i){
+            //     return "translate(" + [ d.x,d.y ] + ")"
+            // })
         });
 
-var player = canvas.selectAll('image .player').data([{'x':Math.random(),'y': Math.random()}]);
+var player = canvas.selectAll('image .player').data([{'x':500,'y':500}]);
 player
     .enter()
     .append('image')
@@ -56,8 +81,34 @@ player
     .attr('width',40)
     .call(drag);
 
+d3.select('body').on('keydown', function(){
+  console.log(d3.event.keyCode);
+  switch(d3.event.keyCode) {
+    case 37:
+      d3.select('.player').attr('x', function(d){
+        return d.x-=8;
+      })
+      break;
+    case 38:
+      d3.select('.player').attr('y', function(d){
+        return d.y-=8;
+      })
+      break;
+    case 39:
+      d3.select('.player').attr('x', function(d){
+        return d.x+=8;
+      })
+      break;
+    case 40:
+      d3.select('.player').attr('y', function(d){
+        return d.y+=8;
+      })
+      break;
+  }
+});
+
 var update = function(){
-  var data = _.map(_.range(0,20),function(value,i){
+  var data = _.map(_.range(0,40),function(value,i){
      return {
       id:i,
       x:Math.random() * width,
@@ -69,8 +120,8 @@ var update = function(){
   //update existing data
   //
   circle
-    // .transition()
-    // .duration(3000)
+    .transition()
+    .duration(2000)
     .attr('x',function(d){
       return d.x;
     })
@@ -81,7 +132,7 @@ var update = function(){
 
       for(var i = 0; i < val.length; i++){
       if(val[i] !== undefined){
-        collide(val[i],player,tester);
+        collide(val[i],player[0][0],collisionVerified);
       }
      }
     })
@@ -101,8 +152,16 @@ var update = function(){
     .attr('y',function(d){
       return d.y;
     });
+    gameMaster();
     // .attr('r',10);
 };
 update();
-setInterval(update, 3000);
+setInterval(update, 2000);
+
+setInterval(function(){
+  currScore+=30;
+  updateScoreBoard();
+},500)
+
+
 
